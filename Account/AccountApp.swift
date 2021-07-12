3f72be05c62854e5c7cffd5fe98bca95563d7b29
Bogdan Petkanich <bogdan.petkanich@nerdpeople.com>
2021-07-12 20:56:31 +0300

    Initial Commit

---
+++ Account/AccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountApp.swift
+//  Account
+//
+//  Created by echo on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
